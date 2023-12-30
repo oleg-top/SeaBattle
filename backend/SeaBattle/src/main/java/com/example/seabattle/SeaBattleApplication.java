f79d5a103ea31102f3a5299a039320b57a688be3
Александр Комаров <komarik@MacBook-Pro-Aleksandr.local>
2023-12-30 19:43:40 +0300

    Добавил бакенд на джаве, мб на ней буду делать

---
+++ backend/SeaBattle/src/main/java/com/example/seabattle/SeaBattleApplication.java
@@ -0,0 +1,13 @@
+package com.example.seabattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaBattleApplication.class, args);
+	}
+
+}
---
+++ backend/SeaBattle/src/main/java/com/example/seabattle/models/User.java
@@ -0,0 +1,20 @@
+package com.example.seabattle.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
