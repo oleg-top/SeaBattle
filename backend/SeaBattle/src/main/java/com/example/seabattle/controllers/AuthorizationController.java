072683a30805dda7e3d6ed77fab37725a7dec88e
oleg-top <a.komarov.post@gmail.com>
2024-01-05 00:07:28 +0300

    Добавил логгер к текущим контроллерам

--- backend/SeaBattle/src/main/java/com/example/seabattle/controllers/AuthorizationController.java
+++ backend/SeaBattle/src/main/java/com/example/seabattle/controllers/AuthorizationController.java
@@ -7,6 +7,7 @@ import com.example.seabattle.models.User;
 import com.example.seabattle.services.UserService;
 import com.example.seabattle.utils.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class AuthorizationController {
 
     private final UserService userService;
@@ -29,10 +31,12 @@ public class AuthorizationController {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authorizationRequest.getUsername(), authorizationRequest.getPassword()));
         } catch (BadCredentialsException e) {
+            log.error(String.format("User with username '%s' wasn't authenticated - Bad credentials", authorizationRequest.getUsername()));
             return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(),"Неправильный логин или пароль"), HttpStatus.UNAUTHORIZED);
         }
         User user = userService.findByUsername(authorizationRequest.getUsername()).get();
         String token = jwtTokenUtils.generateToken(user);
+        log.info(String.format("User '%s' has successfully authenticated", authorizationRequest.getUsername()));
         return ResponseEntity.ok(new AuthorizationResponse(token));
     }
 }
--- backend/SeaBattle/src/main/java/com/example/seabattle/controllers/RegistrationController.java
+++ backend/SeaBattle/src/main/java/com/example/seabattle/controllers/RegistrationController.java
@@ -7,14 +7,19 @@ import com.example.seabattle.models.User;
 import com.example.seabattle.services.UserService;
 import com.example.seabattle.utils.JwtTokenUtils;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class RegistrationController {
 
     private final UserService userService;
@@ -22,12 +27,15 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public ResponseEntity<?> registration(@RequestBody RegistrationRequest registrationRequest) {
-        if (userService.findByUsername(registrationRequest.getUsername()).isPresent())
+        if (userService.findByUsername(registrationRequest.getUsername()).isPresent()) {
+            log.error(String.format("User with username '%s' already exists", registrationRequest.getUsername()));
             return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(),"Пользователь с таким именем уже существует"), HttpStatus.BAD_REQUEST);
+        }
         User user = new User();
         user.setUsername(registrationRequest.getUsername());
         user.setPassword(registrationRequest.getPassword());
         userService.createNewUser(user);
+        log.info(String.format("User '%s' has been created successfully", registrationRequest.getUsername()));
 
         String token = jwtTokenUtils.generateToken(user);
         return ResponseEntity.ok(new RegistrationResponse(token));
